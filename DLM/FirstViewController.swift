615039ac036a8ed7ff32a797a15e8a674aef4eb7
Collin Ruffenach <collin@notionhq.com>
2015-01-29 22:45:23 -0800

    Initial Commit

---
+++ DLM/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  DLM
+//
+//  Created by Collin Ruffenach on 1/29/15.
+//  Copyright (c) 2015 Notion HQ. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
